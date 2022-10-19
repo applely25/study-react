651d7336214a90642e29cc34865b0e7517399699
kimgyuha <kimgyuha25@gmail.com>
2022-10-19 21:41:26 +0900

    [feat] naverCloneCoding img index file

---
+++ signup-naver-clonecoding/src/assets/index.js
@@ -0,0 +1,6 @@
+export { default as allCheckDone_Btn } from "./img/allcheck_done.png";
+export { default as allCheck_Btn } from "./img/allcheck.png";
+export { default as langArrow } from "./img/lang_underArrow.png";
+export { default as naverLogo } from "./img/naverlogo.png";
+export { default as check_Btn } from "./img/checks.png";
+export { default as checkDone_Btn } from "./img/checks_done.png";
