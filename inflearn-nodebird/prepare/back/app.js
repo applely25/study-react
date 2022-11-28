7edc004ef9338ebbac5107253868c7fdd4e538e1
kimgyuha <kimgyuha25@gmail.com>
2022-11-28 17:23:19 +0900

    [feat] 라우터 분리

--- inflearn-nodebird/prepare/back/app.js
+++ inflearn-nodebird/prepare/back/app.js
@@ -1,17 +1,18 @@
-const PORT = 3065;
 const express = require("express");
-
 const app = express();
+const PORT = 3065;
+
+const postRouter = require("./routes/posts");
 
 app.get("/", (req, res) => {
   res.send("Hello Node");
 });
 
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello Api");
 });
 
-app.get("/api/posts", (req, res) => {
+app.get("/posts", (req, res) => {
   res.json([
     {
       id: 1,
@@ -28,22 +29,7 @@ app.get("/api/posts", (req, res) => {
   ]);
 });
 
-app.post("/api/post", (req, res) => {
-  res.json([
-    {
-      id: 1,
-      content: "hello1",
-    },
-  ]);
-});
-
-app.delete("/api/post", (req, res) => {
-  res.json([
-    {
-      id: 1,
-    },
-  ]);
-});
+app.use('/post',postRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
