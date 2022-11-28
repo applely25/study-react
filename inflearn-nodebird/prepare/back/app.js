05d78dc18abf0fc816c9faad345199383be72a9e
kimgyuha <kimgyuha25@gmail.com>
2022-11-28 17:15:43 +0900

    [feat] express 라우팅 하기

--- inflearn-nodebird/prepare/back/app.js
+++ inflearn-nodebird/prepare/back/app.js
@@ -1,13 +1,50 @@
 const PORT = 3065;
-const http = require("http");
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
-  res.write(`<h1>Hello Node</h1>`);
-  res.write(`<h2>Hello Node</h2>`);
-  res.write(`<h3>Hello Node</h3>`);
-  res.write(`<h4>Hello Node</h4>`);
-  res.end("<h5>Hello Node</h5>");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello Node");
 });
-server.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
+
+app.get("/api", (req, res) => {
+  res.send("Hello Api");
+});
+
+app.get("/api/posts", (req, res) => {
+  res.json([
+    {
+      id: 1,
+      content: "hello1",
+    },
+    {
+      id: 2,
+      content: "hello2",
+    },
+    {
+      id: 3,
+      content: "hello3",
+    },
+  ]);
+});
+
+app.post("/api/post", (req, res) => {
+  res.json([
+    {
+      id: 1,
+      content: "hello1",
+    },
+  ]);
+});
+
+app.delete("/api/post", (req, res) => {
+  res.json([
+    {
+      id: 1,
+    },
+  ]);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
